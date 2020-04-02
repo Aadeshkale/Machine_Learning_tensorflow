db41a84d1816e28c4baa828e2fe4591ade4131f6
Aadeshkale <aadeshkale0@gmail.com>
2020-04-02 15:08:13 +0530

    images classificaion using keras fashion_mnist dataset

---
+++ 2.hello_world_computer_vision/img_vision.py
@@ -0,0 +1,60 @@
+'''https://colab.research.google.com/github/lmoroney/dlaicourse/blob/master/Course%201%20-%20Part%204%20-%20Lesson%202%20-%20Notebook.ipynb#scrollTo=6tki-Aro_Uax'''
+import tensorflow as tf
+import numpy as np
+import matplotlib.pyplot as plt
+
+# selecting dataset model from the tf.keras.dataset
+mnist = tf.keras.datasets.fashion_mnist
+
+# loading data in the from of labels
+(training_images, training_labels), (test_images, test_labels) = mnist.load_data()
+
+
+
+# exploring sample images dataset using numpy,mathplotlib
+np.set_printoptions(linewidth=200)
+plt.imshow(training_images[0])
+print(training_labels[0])
+print(training_images[0])
+
+
+# convering data into values into 0 to 1 using float division
+
+training_images = training_images/255.0
+test_images = test_images/255.0
+
+# creating model
+'''
+Sequential: That defines a SEQUENCE of layers in the neural network
+
+Flatten: Remember earlier where our images were a square, when you printed them out? Flatten just takes that square and turns it into a 1 dimensional set.
+
+Dense: Adds a layer of neurons Each layer of neurons need an activation function to tell them what to do. There's lots of options, but just use these for now.
+
+Relu effectively means "If X>0 return X, else return 0" -- so what it does it it only passes values 0 or greater to the next layer in the network.
+
+Softmax takes a set of values, and effectively picks the biggest one, so, for example, if the output of the last layer looks like [0.1, 0.1, 0.05, 0.1, 9.5, 0.1, 0.05, 0.05, 0.05], it saves you from fishing through it looking for the biggest value, and turns it into [0,0,0,0,1,0,0,0,0] -- The goal is to save a lot of coding!
+
+'''
+
+model = tf.keras.models.Sequential([tf.keras.layers.Flatten(),
+                                    tf.keras.layers.Dense(128, activation=tf.nn.relu),
+                                    tf.keras.layers.Dense(10, activation=tf.nn.softmax)])
+
+# compiling model
+
+model.compile(optimizer = tf.optimizers.Adam(),
+              loss = 'sparse_categorical_crossentropy',
+              metrics=['accuracy'])
+# training of model
+model.fit(training_images, training_labels, epochs=5)
+
+# predicting result for test data
+model.evaluate(test_images, test_labels)
+
+
+classifications = model.predict(test_images)
+
+print(classifications[0])
+
+print(test_labels[0])
---
+++ 2.hello_world_computer_vision/readme.md
@@ -0,0 +1,6 @@
+# Beyond Hello World, A Computer Vision Example
+In the previous exercise you saw how to create a neural network that figured out the problem you were trying to solve. This gave an explicit example of learned behavior. Of course, in that instance, it was a bit of overkill because it would have been easier to write the function Y=2x-1 directly, instead of bothering with using Machine Learning to learn the relationship between X and Y for a fixed set of values, and extending that for all values.
+
+But what about a scenario where writing rules like that is much more difficult -- for example a computer vision problem? Let's take a look at a scenario where we can recognize different items of clothing, trained from a dataset containing 10 different types.
+
+refrence:-https://colab.research.google.com/github/lmoroney/dlaicourse/blob/master/Course%201%20-%20Part%204%20-%20Lesson%202%20-%20Notebook.ipynb#scrollTo=qnyTxjK_GbOD
