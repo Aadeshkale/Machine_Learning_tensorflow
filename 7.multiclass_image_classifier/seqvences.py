9f0a2d160e7b2a5722d0df097a1ea631340a579c
Aadeshkale <aadeshkale0@gmail.com>
2020-04-13 14:36:59 +0530

    generating seqvences

---
+++ 7.multiclass_image_classifier/seqvences.py
@@ -0,0 +1,22 @@
+import tensorflow as tf
+from tensorflow.keras.preprocessing.text import Tokenizer
+sentences = [
+    "I love My Parents",
+    "I love My GF",
+    "I love My Friends",
+    "I love My Dpg",
+    "I love My Cats",
+]
+tokenizer = Tokenizer(num_words=100,oov_token="<OOV>") # here oov_token represents the word which is not on dictionary
+tokenizer.fit_on_texts(sentences)
+print("Words dictionary:",tokenizer.index_word)
+# generating sequvences
+sequences = tokenizer.texts_to_sequences(sentences)
+print("sentences generated for training data:",sequences)
+
+
+sample_sentences = [
+    "I really love My Parents",
+    "I love My Job",
+]
+print("sentences generated for sample data",tokenizer.texts_to_sequences(sample_sentences))
--- 8.NLP_build_words_dictionary/readme.md
+++ 8.NLP_build_words_dictionary/readme.md
@@ -4,5 +4,7 @@ NLP Processing using tensorflow
 1.words.py :- generating words dictionary form sentences using tensorflow
 text preprocessor and Tokenizer  
 
+2.seqvences.py :- generating seqvences  form word dictionary using tensorflow
+text preprocessor and Tokenizer   
 
 ----------------------------------------------------------------------------
